205ff53826dba09affb508f47be1cfe23687ce47
jasminbreitwieser <jasmin_breitwieser@web.de>
2022-12-10 01:30:36 +0100

    Update main.py
    set window size
    center window

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ window =tk.Tk()
 window.title("Time Converter")
 window.configure(background="black")
 
-title = tk.Label(
-    text="This Program Helps You Convert Time",
-    foreground="white",  # Set the text color to white
-    background="black"  # Set the background color to black)
-)
-# input_unit = tk.
-title.pack()
+# Set the window size (Width x Height)
+window_width = 600
+window_height = 600
+
+## Center the window
+# get the screen dimension
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+# find the center point
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+# set the position of the window to the center of the screen
+window.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
+
+
 window.mainloop()
