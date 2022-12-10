a314b9d6ff803db1b824d8b0242098a01b2e47e8
jasminbreitwieser <jasmin_breitwieser@web.de>
2022-12-10 01:24:02 +0100

    Update main.py
    window background color and new title

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import tkinter as tk
-window = tk.Tk()
-title = tk.Label(text="Time Converter")
+
+window =tk.Tk()
+window.title("Time Converter")
+window.configure(background="black")
+
+title = tk.Label(
+    text="This Program Helps You Convert Time",
+    foreground="white",  # Set the text color to white
+    background="black"  # Set the background color to black)
+)
+# input_unit = tk.
 title.pack()
 window.mainloop()
